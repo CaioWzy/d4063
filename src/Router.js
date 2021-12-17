39c431f1dbbfb10e79ec190cd62ae15714cd9033
CaioWzy <CaioWzy@gmail.com>
2021-12-16 21:35:13 -0300

    Separa agents em rota /public

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const healthyRouter = require('./healthy/HealthyRouter');
-const agentsRouter = require('./agents/AgentsRouter')
+const agentsRouter = require('./agents/AgentsRouter');
 
 
 const router = express.Router({ mergeParams: true });
+const publicRouter = express.Router({ mergeParams: true });
 
 router.use(healthyRouter);
-router.use('/agents', agentsRouter);
+router.use('/public', publicRouter);
+
+publicRouter.use('/agents', agentsRouter);
 
 module.exports = router;
